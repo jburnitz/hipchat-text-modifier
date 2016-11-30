04aaa0bae10638ebbd57d58904cafc9414a14739
Joseph Burnitz <joseph.burnitz@orbitz.com>
2016-11-30 12:40:07 -0600

    asdf

---
+++ edit-user.js
@@ -0,0 +1,3 @@
+document.getElementById("hc-message-input").addEventListener("hover", function(){
+	document.getElementById("hc-message-input").value = document.getElementById("hc-message-input").value + "<- garbage in, garbage out";
+});
