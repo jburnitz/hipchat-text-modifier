407cb3db968eead634a19837c1f8e29429e24c2e
Joseph Burnitz <joseph.burnitz@orbitz.com>
2016-11-30 14:08:56 -0600

    asdf

--- edit-user.js
+++ edit-user.js
@@ -1,4 +1,4 @@
-asdf = document.getElementById("hc-message-input")
+chat-input = window.parent.document.getElementById("hc-message-input")
 function qwerty(){ alert ("hi");}
 
-asdf.addEventListener("click", qwerty);
+chat-input.addEventListener("click", qwerty);
